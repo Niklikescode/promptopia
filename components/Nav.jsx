88c176426dd532fd371106b6c5a499425f2f8382
Niklikescode <niklikescode@gmail.com>
2024-01-15 21:43:11 +0000

    sign out button

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -2,18 +2,41 @@
 import Link from "next/link";
 import Image from "next/image";
 import { useState, useEffect } from "react";
-import {signIn, signOut, useSession, getProviders} from "next-auth/react";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
+  const isUserLoggedIn = true;
+
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
-        <Image src="nextjs_13_assets/assets/images/logo.svg" width={30} height={30} alt="Promptopia Logo" className="object-contain"
+        <Image
+          src="nextjs_13_assets/assets/images/logo.svg"
+          width={30}
+          height={30}
+          alt="Promptopia Logo"
+          className="object-contain"
         />
         <p className="logo_text">Promptopia </p>
-        </Link>
-    </nav>
-  )
-}
+      </Link>
 
-export default Nav
+      {/* desktop navigation */}
+      <div className="sm:flex hidden">
+        {isUserLoggedIn ? (
+          <div className="flex gap-3 md:gao-5">
+            <Link href="/create-prompt" className="black_btn">
+              Create Post
+            </Link>
+            <button type="button" onClick={signOut} className="outline_btn">
+              Sign Out
+            </button>
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
